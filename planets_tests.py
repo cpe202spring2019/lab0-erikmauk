44e763a40f3231a0f039393dbf8b56d03f38a2ff
Erik Mauk <e.mauk1999@gmail.com>
2019-04-03 10:48:59 -0700

    Added test cases

--- planets_tests.py
+++ planets_tests.py
@@ -12,6 +12,22 @@ class Test_planets(unittest.TestCase):
         #print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
 
+    def test_02(self):
+        sys.stdin = io.StringIO("150")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 57.0 pounds.\nOn Jupiter you would weigh 351.0 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
+    def test_03(self):
+        sys.stdin = io.StringIO("90")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 34.2 pounds.\nOn Jupiter you would weigh 210.6 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
 
 if __name__ == "__main__":
         unittest.main()
